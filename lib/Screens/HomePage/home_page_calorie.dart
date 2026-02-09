e185bc3be5190271333c42c8a9076ff75d87b1c8
Bipin Thapa <yukichin638@gmail.com>
2026-02-09 21:50:48 +0545

    working on homepage calorie tracker

--- lib/Screens/HomePage/home_page_calorie.dart
+++ lib/Screens/HomePage/home_page_calorie.dart
@@ -1,5 +1,6 @@
 import 'package:fitness/standardData.dart';
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class HomePageCalorie extends StatefulWidget {
   const HomePageCalorie({super.key});
@@ -23,6 +24,18 @@ class _HomePageCalorieState extends State<HomePageCalorie> {
     'Nov',
     'Dec',
   ];
+  List<List<dynamic>> details = [
+    [
+      Icon(Icons.restaurant_menu, color: StandardData.iconColor1),
+      "Consumed",
+      "2000",
+    ],
+    [
+      Icon(Icons.local_fire_department, color: StandardData.iconColor2),
+      "Burned",
+      "1000",
+    ],
+  ];
   DateTime now = DateTime.now();
   @override
   Widget build(BuildContext context) {
@@ -43,13 +56,89 @@ class _HomePageCalorieState extends State<HomePageCalorie> {
               borderRadius: BorderRadius.circular(20),
             ),
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
+              padding: const EdgeInsets.all(15),
+              child: Row(
+                spacing: 10,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Net Calories",
-                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 20,
+                      children: [
+                        Text(
+                          "Net Calories",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 18,
+                          ),
+                        ),
+                        CircularPercentIndicator(
+                          radius: 55,
+                          lineWidth: 8,
+                          percent: 0.8,
+                          circularStrokeCap: CircularStrokeCap.round,
+                          backgroundColor: StandardData.mainColor,
+                          progressColor: StandardData.primaryColor,
+                          animation: true,
+                          animationDuration: 1200,
+                          center: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              text: "80%\n",
+                              children: [
+                                TextSpan(
+                                  text: "done",
+                                  style: TextStyle(fontSize: 10),
+                                ),
+                              ],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 24),
+                      child: Column(
+                        spacing: 10,
+                        children: [
+                          ...details.map((item) {
+                            return Row(
+                              spacing: 5,
+                              children: [
+                                item[0],
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      item[1],
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                    Text(
+                                      item[2],
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            );
+                          }),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
--- lib/standardData.dart
+++ lib/standardData.dart
@@ -7,7 +7,10 @@ class StandardData {
   //colors part
   static const Color primaryColor = Colors.deepPurpleAccent;
   static const Color backgroundColor1 = Color(0xFF292929);
+  static const Color mainColor = Color(0xFF151515);
   static const Color buttonColor1 = Color(0xFF333333);
+  static const Color iconColor1 = Colors.blueAccent;
+  static const Color iconColor2 = Colors.orangeAccent;
 
   //url
   static const String baseUrl = "http://192.168.1.64:8080";
