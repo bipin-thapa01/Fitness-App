7c55da1f0db1e7a0ec9fb13210da356acd689ab8
Bipin Thapa <yukichin638@gmail.com>
2026-02-13 22:48:23 +0545

    working on bottom navigation of dashboard

--- lib/Screens/HomePage/home_page.dart
+++ lib/Screens/HomePage/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:fitness/Screens/HomePage/home_page_appbar.dart';
 import 'package:fitness/Screens/HomePage/home_page_calorie.dart';
 import 'package:fitness/Screens/LoginPage/login_page.dart';
+import 'package:fitness/standardData.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -42,6 +43,63 @@ class _HomePageState extends State<HomePage> {
           HomePageAppbar(data: data),
           SliverToBoxAdapter(child: HomePageCalorie()),
         ],
+      ),
+      floatingActionButton: IconButton(
+        iconSize: 30,
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (context) {
+              return DraggableScrollableSheet(
+                initialChildSize: 0.5,
+                maxChildSize: 0.5,
+                expand: false,
+                builder: (context, scrollController) {
+                  return Container(child: CustomScrollView(slivers: []));
+                },
+              );
+            },
+          );
+        },
+        icon: Icon(Icons.qr_code_scanner),
+        style: IconButton.styleFrom(backgroundColor: StandardData.primaryColor),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        height: 65,
+        color: Theme.of(context).scaffoldBackgroundColor,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 6,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.dashboard),
+                Text("Dashboard", style: TextStyle(fontSize: 12)),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.fitness_center),
+                Text("Workout", style: TextStyle(fontSize: 12)),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.calendar_month),
+                Text("Progress", style: TextStyle(fontSize: 12)),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.settings),
+                Text("Settings", style: TextStyle(fontSize: 12)),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
